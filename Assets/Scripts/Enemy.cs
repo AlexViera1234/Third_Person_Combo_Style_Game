8fe376e21fd9c72707b048f6803aa48bc2fec910
Alex Viera <viera.alex95@gmail.com>
2019-09-30 12:00:30 -0400

    added all unity files

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    GameObject enemy;
+    NavMeshAgent agent;
+    public int LifePoints = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/PlayerControllerForReal.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerForReal : MonoBehaviour
+{
+    
+    Animator anim;
+    
+    
+    void Start()
+    { 
+        anim = GetComponent<Animator>(); 
+    }
+
+ 
+    void Update()
+    { 
+        if (anim == null) return;
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        Move(x, y);
+        Attack();
+    }
+
+    private void Move(float x, float y)
+    {
+        anim.SetFloat("hSpeed", x);
+        anim.SetFloat("vSpeed", y);
+
+    }
+
+    private void Attack()
+    {
+        if (Input.GetButtonDown("Rpunch"))
+        {
+            anim.SetTrigger("Rpunch");
+        }
+
+        if (Input.GetButtonDown("Lpunch"))
+        {
+            anim.SetTrigger("Lpunch");
+        }
+
+        if (Input.GetButtonDown("Ultra"))
+        {
+            anim.SetTrigger("Ultra");
+        }
+    }
+    private void FixedUpdate()
+    {
+       // Attack();
+    }
+}
